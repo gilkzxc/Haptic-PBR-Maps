533f8c3fbb02ce9f8111931f8916e3d8494dca75
gilkzxc <37815312+gilkzxc@users.noreply.github.com>
2024-03-27 22:36:55 +0200

    Scaling v0.1
    - Our custom DMS (Apple's Material segmentation model) interface:
    Making layouts and base for connecting with other models as part of the whole system.
    - Achieved almost perfect histograms linked on the right of the predictions.
    
    *** Important late notice: All outputs are images and maps in 512px scaling. As that was the parameters handled in the original inference.py

--- ml_dms_dataset/dms_interface.py
+++ ml_dms_dataset/dms_interface.py
@@ -1,7 +1,7 @@
 # interface with DMS
 import inference
 import argparse
-from ast import If
+from ast import If, Try
 import torchvision.transforms as TTR
 import os
 import glob
@@ -13,12 +13,27 @@ import torch
 import math
 from PIL import Image
 from collections import deque
+import matplotlib.pyplot as plt
 
 pretrained_path = "./DMS46_v1.pt"
 
 random.seed(112)
 def is_valid_img(img):
     return True
+
+def rgba2rgb( rgba, background=(255,255,255) ):
+    row, col, ch = rgba.shape
+    if ch == 3:
+        return rgba
+    assert ch == 4, 'RGBA image has 4 channels.'
+    rgb = np.zeros( (row, col, 3), dtype='float32' )
+    r, g, b, a = rgba[:,:,0], rgba[:,:,1], rgba[:,:,2], rgba[:,:,3]
+    a = np.asarray( a, dtype='float32' ) / 255.0
+    R, G, B = background
+    rgb[:,:,0] = r * a + (1.0 - a) * R
+    rgb[:,:,1] = g * a + (1.0 - a) * G
+    rgb[:,:,2] = b * a + (1.0 - a) * B
+    return np.asarray( rgb, dtype='uint8' )
 
 class infered_image:
     def __init__(self,image_path):
@@ -34,13 +49,14 @@ class infered_image:
         new_w = math.ceil(scale * w)
         img = Image.fromarray(img).resize((new_w, new_h), Image.Resampling.LANCZOS)
         img = np.array(img)
-        if inference.is_valid_img(img):
+        if is_valid_img(img):
             self.img = img
         else:
             self.img = None
         self.estimated_colors = None
         self.stats = {"num_of_pixels":0,"materials":{}}
         self.material_mapping = []
+        self.histogram_img_plot = None
     
     def was_predicted(self):
         if (not isinstance(self.stats,dict)) or self.stats == {"num_of_pixels":0,"materials":{}}:
@@ -51,9 +67,9 @@ class infered_image:
 
 
     def estimation(self,is_cuda,model,mean,std):
-        if self.img == None:
+        if not isinstance(self.img,np.ndarray):
             return None
-        if self.estimated_colors != None:
+        if isinstance(self.estimated_colors,np.ndarray):
             return self.estimated_colors
         image = torch.from_numpy(self.img.transpose((2, 0, 1))).float()
         image = TTR.Normalize(mean, std)(image)
@@ -67,7 +83,7 @@ class infered_image:
         return self.estimated_colors
         
     def fetch_estimation(self):
-        if self.img == None or self.estimated_colors == None:
+        if not isinstance(self.img,np.ndarray) or not isinstance(self.estimated_colors,np.ndarray):
             return None
         if self.was_predicted():
             return self.material_mapping
@@ -89,8 +105,39 @@ class infered_image:
         self.stats = stats
         self.material_mapping = material_mapping
         return material_mapping
-
-
+    
+    def histogram(self):
+        if not self.was_predicted():
+            return None
+        # Extracting RGB values and counts of the different materials.
+        names = [self.stats['materials'][material_index]['name'] for material_index in self.stats["materials"]]
+        colors = [self.stats['materials'][material_index]['rgb_color'] for material_index in self.stats["materials"]]
+        counts = [self.stats['materials'][material_index]['num_of_pixels'] for material_index in self.stats["materials"]]
+        
+        # Plotting histogram
+        fig, ax = plt.subplots()
+        fig.set_size_inches((d/fig.dpi for d in self.img.shape[:2][::-1]))
+        ax.bar(names, counts, color=[(r / 255, g / 255, b / 255) for (r, g, b) in colors])
+        ax.set_ylabel('pixel count')
+        ax.set_xlabel('Materials')
+        ax.set_title('Pixel histogram for each material')
+        #fig.set_size_inches((d/fig.dpi for d in self.img.shape[:2][::-1]))
+        plt.show()
+        fig.canvas.draw()
+        self.histogram_img_plot = rgba2rgb(np.array(fig.canvas.buffer_rgba()))
+        return self.histogram_img_plot
+        
+    
+    def write_concate_results(self,output_folder_path):
+        if self.was_predicted():
+            original_img = np.uint8(self.img[..., ::-1])
+            if isinstance(original_img,np.ndarray):
+                estimated_colors = self.estimated_colors[..., ::-1]
+                if isinstance(estimated_colors,np.ndarray):
+                    histogram = self.histogram_img_plot[..., ::-1]
+                    if isinstance(histogram,np.ndarray):
+                        stacked_img = np.concatenate((original_img, estimated_colors,histogram), axis=1)
+                        cv2.imwrite(f'{output_folder_path}/{os.path.splitext(os.path.basename(self.image_path))[0]}.png',stacked_img,)
 
 class infering_pipeline:
     def __init__(self,model_path,output_folder_path,parameters = inference.parameters):
@@ -104,14 +151,50 @@ class infering_pipeline:
         self.pipeline_to_infer = deque([])
         self.pipeline_infered = deque([])
     def insert_into_infer(self, image_path):
-        self.pipeline.append(infered_image(image_path))
+        self.pipeline_to_infer.append(infered_image(image_path))
     def run_pipeline(self):
         #something
         os.makedirs(self.output_folder_path, exist_ok=True)
         while len(self.pipeline_to_infer) > 0:
             head = self.pipeline_to_infer.popleft()
+            print(f"Image {head.image_path} : Running Material Segmentation model...")
             estimation_color = head.estimation(self.is_cuda,self.model,self.mean,self.std)
             material_mapping = head.fetch_estimation()
+            print(f"Image {head.image_path} stats:\n{head.stats}")
+            np.histogram = head.histogram()
+            head.write_concate_results(self.output_folder_path)
+            print(f"Image {head.image_path} : Saved result in desired output file.")
             self.pipeline_infered.append(head)
-            stacked_img = np.concatenate((np.uint8(head.img), estimation_color), axis=1)
-            cv2.imwrite(f'{self.output_folder_path}/{os.path.splitext(os.path.basename(head.image_path))[0]}.png',stacked_img,)
+            
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--pretrained_path',
+        type=str,
+        default='',
+        help='path to the pretrained model',
+    )
+    parser.add_argument(
+        '--image_folder',
+        type=str,
+        default='',
+        help='overwrite the data_path for local runs',
+    )
+    parser.add_argument(
+        '--output_folder',
+        type=str,
+        default='',
+        help='overwrite the data_path for local runs',
+    )
+    args = parser.parse_args()
+    images_list = glob.glob(f'{args.image_folder}/*')
+    print("Initiating pipelines.")
+    test_pipe = infering_pipeline(args.pretrained_path,args.output_folder)
+    print("Inserting images into pipelines.")
+    for image_path in images_list:
+        print(image_path)
+        test_pipe.insert_into_infer(image_path)
+    print("Begin workload...")
+    test_pipe.run_pipeline()
+    print("Done...")
+    
